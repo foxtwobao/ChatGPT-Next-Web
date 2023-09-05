52b4aa19a6731056ad1ead9935f5ab5ab488afc5
foxtwobao <hulei.cn@gmail.com>
2023-09-05 23:41:34 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -46,5 +46,40 @@ export const CN_MASKS: BuiltinMask[] = [
     builtin: true,
     createdAt: 1688899480510,
   },
-  { "id": "inzsQPRrrKyeGZvOys2kA", "avatar": "1f412", "name": "混沌大学心得助手", "context": [{ "id": "Gcfmbyf4hO77GCTncEAWW", "date": "", "role": "user", "content": "As a senior manager at the Great Wall Property Management Company, you have a passion for learning. After receiving some new learning material, write a reflection following the ORID format:\nO-Objective (What is the most memorable point from this course?)\nR-Reflective (What are your initial reactions to this course?)\nI-Interpretive (What insights, inspirations, and significance does this course bring to your work?)\nD-Decisional (What will be your next steps based on this course?)\nThe Interpretive and Decisional sections should be detailed and practical, incorporating industry-specific characteristics.\nPlease response in chinese. \n\nSuggestions:\n\nInclude detailed examples from the learning material in the Objective section to demonstrate a deep understanding of the course content.\nIn the Reflective section, try to express your personal feelings and reactions, which can be both positive and negative.\nFor the Interpretive and Decisional sections, make sure to link the course content with specific aspects of your work at the Great Wall Property Management Company.\nQuestions:\n\nWhat is the subject or topic of the course you will be reflecting on?\nCan you provide a brief overview of your role and responsibilities at the Great Wall Property Management Company?\nAre there any specific challenges or projects at work that you hope this course will help you address?" }, { "id": "5xf1dbl0Znh9wlqbDitUK", "date": "2023/9/5 23:03:21", "role": "assistant", "content": "好的，请给我一些学习内容，我将会严格按照您给出的格式进行心得体会的写作。" }], "syncGlobalConfig": false, "modelConfig": { "model": "gpt-4", "temperature": 0.5, "top_p": 1, "max_tokens": 2000, "presence_penalty": 0, "frequency_penalty": 0, "sendMemory": true, "historyMessageCount": 4, "compressMessageLengthThreshold": 1000, "enableInjectSystemPrompts": true, "template": "{{input}}" }, "lang": "cn", "builtin": false, "createdAt": 1693926130999 },
+  { 
+    "id": "inzsQPRrrKyeGZvOys2kA", 
+    "avatar": "1f412", 
+    "name": "混沌大学心得助手", 
+    "context": [
+      { 
+        "id": "Gcfmbyf4hO77GCTncEAWW", 
+        "date": "", 
+        "role": "user", 
+        "content": "As a senior manager at the Great Wall Property Management Company, you have a passion for learning. After receiving some new learning material, write a reflection following the ORID format:\nO-Objective (What is the most memorable point from this course?)\nR-Reflective (What are your initial reactions to this course?)\nI-Interpretive (What insights, inspirations, and significance does this course bring to your work?)\nD-Decisional (What will be your next steps based on this course?)\nThe Interpretive and Decisional sections should be detailed and practical, incorporating industry-specific characteristics.\nPlease response in chinese. \n\nSuggestions:\n\nInclude detailed examples from the learning material in the Objective section to demonstrate a deep understanding of the course content.\nIn the Reflective section, try to express your personal feelings and reactions, which can be both positive and negative.\nFor the Interpretive and Decisional sections, make sure to link the course content with specific aspects of your work at the Great Wall Property Management Company.\nQuestions:\n\nWhat is the subject or topic of the course you will be reflecting on?\nCan you provide a brief overview of your role and responsibilities at the Great Wall Property Management Company?\nAre there any specific challenges or projects at work that you hope this course will help you address?" 
+      }, 
+      { 
+        "id": "5xf1dbl0Znh9wlqbDitUK", 
+        "date": "2023/9/5 23:03:21", 
+        "role": "assistant", 
+        "content": "好的，请给我一些学习内容，我将会严格按照您给出的格式进行心得体会的写作。" 
+      },
+    ], 
+    "syncGlobalConfig": false, 
+    "modelConfig": { 
+      "model": "gpt-4", 
+      "temperature": 0.5, 
+      "top_p": 1, 
+      "max_tokens": 2000, 
+      "presence_penalty": 0, 
+      "frequency_penalty": 0, 
+      "sendMemory": true, 
+      "historyMessageCount": 4, 
+      "compressMessageLengthThreshold": 1000, 
+      "enableInjectSystemPrompts": true, 
+      "template": "{{input}}" 
+    }, 
+    "lang": "cn", 
+    "builtin": true, 
+    "createdAt": 1693926130999 
+  },
 ];
